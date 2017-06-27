acc72b9fb7b456860911bde11207737c101766b4
JavierSalazarG <javizz.hjsg@gmail.com>
2017-06-27 13:11:51 -0700

    cambios menores, indents y asi

--- application/views/compiler/create.php
+++ application/views/compiler/create.php
@@ -1,15 +1,15 @@
 <div class="container" style="margin-top:32px">
-<?php echo validation_errors() ;?>
-<?php echo form_open('compilerC/create');?>
-<h2>Escribe que insertar</h2>
-<input type="text" name="insert"class="form-control" required></input>
-                                                <select name="tipo" class="form-control">
-                                                  <option value="Numero">Numero</option>
-                                                  <option value="Funcion">Funcion</option>
-                                                  <option value="Caracter">Char</option>
-                                                  <option value="Instruccion">Instruccion</option>
-                                                  <<option value="Operador">Operador</option>
-                                                </select>
-<center><input type="submit" name="sumbit" class="btn btn-primary btn-lg btn-block" value="Aceptar"></input></center><br><br>
+    <?php echo validation_errors() ;?>
+    <?php echo form_open('compilerC/create');?>
+    <h2>Escribe que insertar</h2>
+    <input type="text" name="insert"class="form-control" required></input>
+    <select name="tipo" class="form-control">
+        <option value="Numero">Numero</option>
+        <option value="Funcion">Funcion</option>
+        <option value="Caracter">Char</option>
+        <option value="Instruccion">Instruccion</option>
+        <<option value="Operador">Operador</option>
+    </select>
+    <center><input type="submit" name="sumbit" class="btn btn-primary btn-lg btn-block" value="Aceptar"></input></center><br><br>
 </form>
 </div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,9 +1,11 @@
 <html>
         <head>
                 <title>Compilador</title>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         </head>
         <body>
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
           <nav class="navbar navbar-default navbar-fixed-top">
             <div class="container">
               <div class="navbar-header">
