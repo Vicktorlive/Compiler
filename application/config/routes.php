e009d73bc090512533a61c93e402112d8a556cb5
JavierSalazarG <javizz.hjsg@gmail.com>
2017-07-04 08:42:02 -0700

    cleaned up

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,9 +49,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-/*$route['compiler/create'] = 'compiler/create';
+$route['compiler/create'] = 'compiler/create';
 $route['compiler/(:any)'] = 'compiler/view/$1';
 $route['default_controller'] = 'compilerC/view';
 $route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;*/
+$route['translate_uri_dashes'] = FALSE;
